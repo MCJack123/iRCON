25e42a4a3e8b00e12c0015364efd1d3d5a09ec01
MCJack123 <jackmacwindowslinux@gmail.com>
2021-10-25 14:18:36 -0400

    Initial Commit

---
+++ iRCON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iRCON
+//
+//  Created by Jack Bruienne on 10/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
